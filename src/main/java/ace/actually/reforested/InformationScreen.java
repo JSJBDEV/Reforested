9b46c3037c9e48331227b16e98941086e5973b92
JSJBDEV <acrodynasty@gmail.com>
2024-07-25 14:41:10 +0100

    information screen effectively done, also an icon I guess (and fixing some admin stuff)

--- src/main/java/ace/actually/reforested/InformationScreen.java
+++ src/main/java/ace/actually/reforested/InformationScreen.java
@@ -3,26 +3,138 @@ package ace.actually.reforested;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.nbt.visitor.NbtTextFormatter;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class InformationScreen extends Screen {
+
+    public static HashMap<String,String> PAGES = new HashMap<>();
+    static
+    {
+        PAGES.put("Trees","information.reforested.trees");
+        PAGES.put("Bees","information.reforested.bees");
+        PAGES.put("Compartments","information.reforested.compartments");
+        PAGES.put("Multi Farm","information.reforested.multi_farm");
+        PAGES.put("Peat","information.reforested.peat");
+        PAGES.put("Centrifuge","information.reforested.centrifuge");
+    }
+    private static HashMap<String,String[]> REFORMATTED_PAGES = new HashMap<>();
+
     protected InformationScreen(Text title) {
         super(title);
     }
 
+    private void reformatPages()
+    {
+        for(String page: PAGES.keySet())
+        {
+            String[] split = Text.translatable(PAGES.get(page)).getString().split(" ");
+            List<String> strings = new ArrayList<>();
+            String current = "";
+            for(String part: split)
+            {
+                if(part.equals("\n"))
+                {
+                    strings.add(current);
+                    current="";
+                }
+                else if(current.length()+part.length()<30)
+                {
+                    current+=part+" ";
+                }
+                else
+                {
+                    strings.add(current);
+                    current=part+" ";
+                }
+            }
+            strings.add(current);
+            REFORMATTED_PAGES.put(page,strings.toArray(new String[0]));
+        }
+    }
+
+    private TextFieldWidget search;
+    private List<ButtonWidget> pageWidgets = new ArrayList<>();
+
+    private int pageOffset = 0;
     @Override
     protected void init() {
         super.init();
-        this.addDrawableChild(ButtonWidget.builder(Text.of("Trees"),a->{}).dimensions(2 + width / 4,25,100,20).build());
+        reformatPages();
+        //this.addDrawableChild(ButtonWidget.builder(Text.of("Bees"),a->{}).dimensions(3 + width / 4,62,75,20).build());
+
+        search=this.addDrawableChild(new TextFieldWidget(textRenderer,3 + width / 4,42,75,20,Text.of("...")));
+        int i = 1;
+        for(String page: REFORMATTED_PAGES.keySet())
+        {
+            pageWidgets.add(this.addDrawableChild(ButtonWidget.builder(Text.of(page),a->
+                    {
+                        PAGE_OPEN=REFORMATTED_PAGES.get(page);
+                        pageOffset=0;
+                    } )
+                    .dimensions(3 + width / 4,(20*i)+42,75,20).build()));
+            i++;
+        }
+        this.addDrawableChild(ButtonWidget.builder(Text.of(">"),a-> pageOffset++)
+                .dimensions(153 + width / 4,80+height/2,20,20).build());
+        this.addDrawableChild(ButtonWidget.builder(Text.of("<"),a-> pageOffset--)
+                .dimensions(133 + width / 4,80+height/2,20,20).build());
     }
+
+    private String[] PAGE_OPEN = new String[0];
+
+
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         super.render(context, mouseX, mouseY, delta);
         context.drawText(textRenderer,"Reforested - Information",2 + width / 4,25, Color.WHITE.getRGB(),true);
+
+        if(pageOffset<0)
+        {
+            pageOffset=0;
+        }
+
+        int line = 0;
+        for (int i = pageOffset*14; i < 14*(1+pageOffset); i++) {
+            if(i<PAGE_OPEN.length)
+            {
+                context.drawText(textRenderer,Text.translatable(PAGE_OPEN[i]),81 + width / 4,45+(line*10), Color.WHITE.getRGB(),true);
+            }
+            line++;
+        }
+        int i = 1;
+        for(ButtonWidget widget: pageWidgets)
+        {
+            if(widget.getMessage().getString().toLowerCase().contains(search.getText().toLowerCase()))
+            {
+                widget.visible=true;
+                widget.setY((20*i)+42);
+                i++;
+            }
+            else if(Text.translatable(PAGES.get(widget.getMessage().getString())).getString().toLowerCase().contains(search.getText().toLowerCase()))
+            {
+                widget.visible=true;
+                widget.setY((20*i)+42);
+                i++;
+            }
+            else
+            {
+                widget.visible=false;
+            }
+        }
+
+
     }
 
     private static final Identifier TEXTURE = Identifier.of("reforested","textures/gui/information.png");
--- src/main/java/ace/actually/reforested/datagen/RLanguageProvider.java
+++ src/main/java/ace/actually/reforested/datagen/RLanguageProvider.java
@@ -75,12 +75,21 @@ public class RLanguageProvider extends FabricLanguageProvider {
 
         translationBuilder.add("thing.reforested.bee","Bee");
 
+        translationBuilder.add("category.reforested","Reforested");
+        translationBuilder.add("key.reforested.consider","Open information about Reforested");
+
 
         translationBuilder.add("text.reforested.typeof","This is a ");
         translationBuilder.add("text.reforested.bee.temperature","Preferred Temperature: ");
         translationBuilder.add("text.reforested.bee.humidity","Likes Humidity: ");
         translationBuilder.add("text.reforested.bee.natural","Is Natural: ");
 
+        translationBuilder.add("information.reforested.bees","Bees are neat. \n - \n Around the world, ask you would expect in vanilla you will find hives, these hives no longer contain normal minecraft bees but special Reforested variants, they have working conditions that need to be met, but some bees can produce fascinating things in their combs (you need a centrifuge to process combs) and honey; to get at this new stuff, you will need your bees to be in an apiary \n - \n two bees may breed if they are in a hive with a free space");
+        translationBuilder.add("information.reforested.trees","Tree breeding is one of the core tenants of Reforested \n - \n To start you will need to craft yourself some Tree Canes and place them with two saplings either side of them, after some time, before the sapling grows into a tree, one of the saplings may mutate into another type of sapling");
+        translationBuilder.add("information.reforested.compartments","Compartments are a bit like chests, but they have tabs so you can organise your stuff more adequately and a search bar so you can find the stuff you supposedly organised, one other advantage of compartments is you can just shift-click items into them and they will go into the next available slot in any tab (starting from tab 1) which is pretty neat \n - \n Think of them as early/mid game organisation");
+        translationBuilder.add("information.reforested.multi_farm","The Multi-Farm is block that can be used to plant and harvest saplings and crops \n - \n the farm will collect any items around it and replant as it receives things to do so, it requires power to function, and you are probably best off making 2 peat-fired engines for this");
+        translationBuilder.add("information.reforested.peat","Peat is an item used in peat-fired engines to make power, it is obtained by placing Block o' Bog near water and waiting until it matures");
+        translationBuilder.add("information.reforested.centrifuge","The centrifuge is an industry book that has 1 input and 9 outputs, it is mainly used to process combs into their constituent parts. The block requires a small amount of power to function, so only 1 peat-fired engine is needed to maintain it");
 
     }
 }
